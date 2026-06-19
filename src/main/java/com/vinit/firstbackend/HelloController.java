57d76f1a18dc1b57792f7f99c0d3d652c4e3df84
VinitKumar079 <vinitkumar2020079@gmail.com>
2026-06-19 21:41:29 +0530

    Day 3 completed

--- src/main/java/com/vinit/firstbackend/HelloController.java
+++ src/main/java/com/vinit/firstbackend/HelloController.java
@@ -2,6 +2,7 @@ package com.vinit.firstbackend;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class HelloController {
@@ -14,6 +15,16 @@ public class HelloController {
     @GetMapping("/student")
     public Student student() {
         return new Student("Vinit", "Spring Boot");
+    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name;
+    }
+
+    @GetMapping("/college/{collegeName}")
+    public String college(@PathVariable String collegeName) {
+        return "My college is " + collegeName;
     }
 
 }
