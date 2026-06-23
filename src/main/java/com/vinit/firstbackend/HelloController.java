f6691cb4bd9dc0cae7d324cd173e63d4a670ee0f
VinitKumar079 <vinitkumar2020079@gmail.com>
2026-06-23 17:04:09 +0530

    Day 7 - CRUD Introduction and In-Memory Storage

--- src/main/java/com/vinit/firstbackend/HelloController.java
+++ src/main/java/com/vinit/firstbackend/HelloController.java
@@ -4,59 +4,77 @@ import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello Vinit";
-    }
+    ArrayList<Student> studentsList = new ArrayList<>();
 
-    @GetMapping("/student")
-    public Student student() {
-        return new Student("Vinit", "Spring Boot");
-    }
+    // @GetMapping("/hello")
+    // public String hello() {
+    // return "Hello Vinit";
+    // }
 
-    @GetMapping("/hello/{name}")
-    public String sayHello(@PathVariable String name) {
-        return "Hello " + name;
-    }
+    // @GetMapping("/student")
+    // public Student student() {
+    // return new Student("Vinit", "Spring Boot");
+    // }
 
-    @GetMapping("/college/{collegeName}")
-    public String college(@PathVariable String collegeName) {
-        return "My college is " + collegeName;
-    }
+    // @GetMapping("/hello/{name}")
+    // public String sayHello(@PathVariable String name) {
+    // return "Hello " + name;
+    // }
 
-    @GetMapping("/add")
-    public int add(
-            @RequestParam int a,
-            @RequestParam int b) {
-        return a + b;
-    }
+    // @GetMapping("/college/{collegeName}")
+    // public String college(@PathVariable String collegeName) {
+    // return "My college is " + collegeName;
+    // }
 
-    @GetMapping("/multiply")
-    public int multiply(
-            @RequestParam int a,
-            @RequestParam int b) {
-        return a * b;
-    }
+    // @GetMapping("/add")
+    // public int add(
+    // @RequestParam int a,
+    // @RequestParam int b) {
+    // return a + b;
+    // }
 
-    @PostMapping("/student")
-    public Student createStudent(@RequestBody Student student) {
-        return student;
+    // @GetMapping("/multiply")
+    // public int multiply(
+    // @RequestParam int a,
+    // @RequestParam int b) {
+    // return a * b;
+    // }
+
+    // @PostMapping("/student")
+    // public Student createStudent(@RequestBody Student student) {
+    // return student;
+    // }
+
+    // @GetMapping("/students")
+    // public ArrayList<Student> students() {
+    // ArrayList<Student> studentsList = new ArrayList<>();
+    // studentsList.add(new Student("Vinit", "Spring Boot"));
+    // studentsList.add(new Student("Alice", "Java"));
+    // studentsList.add(new Student("Bob", "Python"));
+    // return studentsList;
+    // }
+
+    @GetMapping("/load")
+    public String loadData() {
+
+        studentsList.add(new Student("Vinit", "Spring Boot"));
+        studentsList.add(new Student("Rahul", "Java"));
+        studentsList.add(new Student("Aman", "React"));
+        studentsList.add(new Student("Rohit", "Python"));
+
+        return "Students Loaded";
     }
 
     @GetMapping("/students")
-    public ArrayList<Student> students() {
-        ArrayList<Student> studentsList = new ArrayList<>();
-        studentsList.add(new Student("Vinit", "Spring Boot"));
-        studentsList.add(new Student("Alice", "Java"));
-        studentsList.add(new Student("Bob", "Python"));
+    public ArrayList<Student> getStudents() {
         return studentsList;
     }
 }
