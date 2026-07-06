3009033c7d272d9b2d55a58f3415f8eed2dbb7e7
VinitKumar079 <vinitkumar2020079@gmail.com>
2026-07-06 21:31:32 +0530

    Day 17 - Global Exception Handling and Custom Error Responses

---
+++ src/main/java/com/vinit/firstbackend/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.vinit.firstbackend.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(
+            MethodArgumentNotValidException ex) {
+
+        ErrorResponse error = new ErrorResponse(
+                "Validation Failed",
+                400);
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
