564c8a2c8fa6302610f3afd93e0aa54bd49cd391
VinitKumar079 <vinitkumar2020079@gmail.com>
2026-07-18 09:26:01 +0530

    Day 29 - Added Lombok

--- src/main/java/com/vinit/firstbackend/Student.java
+++ src/main/java/com/vinit/firstbackend/Student.java
@@ -1,15 +1,23 @@
 package com.vinit.firstbackend;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
-import java.util.List;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.CascadeType;
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
 
     @Id
@@ -22,60 +30,14 @@ public class Student {
     @NotBlank(message = "Course cannot be empty")
     private String course;
 
-    public Student() {
-
-    }
-
-    public Student(String name, String course) {
-
-        this.name = name;
-        this.course = course;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getCourse() {
-        return course;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
-    // public Object getEmail() {
-    //     // TODO Auto-generated method stub
-    //     throw new UnsupportedOperationException("Unimplemented method 'getEmail'");
-    // }
-
-    // public void setEmail(Object email) {
-    //     // TODO Auto-generated method stub
-    //     throw new UnsupportedOperationException("Unimplemented method 'setEmail'");
-    // }
-
     @OneToMany(
-        mappedBy = "student",
-        cascade = CascadeType.ALL)
-private List<Course> courses;
-    public List<Course> getCourses() {
-    return courses;
-}
+            mappedBy = "student",
+            cascade = CascadeType.ALL)
+    private List<Course> courses;
 
-public void setCourses(
-        List<Course> courses) {
-    this.courses = courses;
-}   
+    // Constructor used in previous code
+    public Student(String name, String course) {
+        this.name = name;
+        this.course = course;
+    }
 }
